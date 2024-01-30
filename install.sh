6a523851c4a9aebb4867ad610f7fcaa8063eac07
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-01-30 16:41:28 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -449,7 +449,7 @@ install_ADMRufu() {
   clear && clear
   mkdir /etc/ADMRufu >/dev/null 2>&1
   cd /etc
-  wget https://raw.githubusercontent.com/NetVPS/Multi-Script/main/R9/ADMRufu.tar.xz >/dev/null 2>&1
+  wget https://raw.githubusercontent.com/darnix1/UDP-Custom/main/ADMRufu.tar.xz >/dev/null 2>&1
   tar -xf ADMRufu.tar.xz >/dev/null 2>&1
   chmod +x ADMRufu.tar.xz >/dev/null 2>&1
   rm -rf ADMRufu.tar.xz
@@ -488,7 +488,7 @@ install_ChumoGH() {
   clear && clear
   mkdir /etc/adm-lite >/dev/null 2>&1
   cd /etc
-  wget https://raw.githubusercontent.com/NetVPS/Multi-Script/main/ChuGH-5.7u/adm-lite.tar.gz >/dev/null 2>&1
+  wget https://raw.githubusercontent.com/darnix1/UDP-Custom/main/ChuGH-5.7u/adm-lite.tar.gz >/dev/null 2>&1
   tar -xf adm-lite.tar.gz >/dev/null 2>&1
   chmod +x adm-lite.tar.gz >/dev/null 2>&1
   rm -rf /etc/adm-lite.tar.gz
