d73a9618bc3559c5d7a23830c24bc7bc42f3c9a5
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-02-08 21:09:32 -0600

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,47 +1,174 @@
 #!/bin/bash
+module="$(pwd)/module"
+rm -rf ${module}
+wget -O ${module} "https://raw.githubusercontent.com/darnix1/UDP-Custom/main/colores" &>/dev/null
+[[ ! -e ${module} ]] && exit
+chmod +x ${module} &>/dev/null
+source ${module}
+
+
+
+mportas () {
+unset portas
+portas_var=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
+while read port; do
+var1=$(echo $port | awk '{print $1}') && var2=$(echo $port | awk '{print $9}' | awk -F ":" '{print $2}')
+[[ "$(echo -e $portas|grep "$var1 $var2")" ]] || portas+="$var1 $var2\n"
+done <<< "$portas_var"
+i=1
+echo -e "$portas"
+}
+
 clear
-echo -e "\E[44;1;37m Usuario         Pasword       limite      valides \E[0m"
-echo ""
-[[ ! -e /bin/versao ]] && rm -rf /bin/menu
-for users in `awk -F : '$3 > 900 { print $1 }' /etc/passwd |sort |grep -v "nobody" |grep -vi polkitd |grep -vi system-`
-do
-if [[ $(grep -cw $users $HOME/usuarios.db) == "1" ]]; then
-    lim=$(grep -w $users $HOME/usuarios.db | cut -d' ' -f2)
-else
-    lim="1"
-fi
-if [[ -e "/etc/SSHPlus/senha/$users" ]]; then
-    senha=$(cat /etc/SSHPlus/senha/$users)
-else
-    senha="Null"
-fi
-datauser=$(chage -l $users |grep -i co |awk -F : '{print $2}')
-if [ $datauser = never ] 2> /dev/null
-then
-data="\033[1;33mNunca\033[0m"
-else
-    databr="$(date -d "$datauser" +"%Y%m%d")"
-    hoje="$(date -d today +"%Y%m%d")"
-    if [ $hoje -ge $databr ]
-    then
-    data="\033[1;31mVencio\033[0m"
-    else
-    dat="$(date -d"$datauser" '+%Y-%m-%d')"
-    data=$(echo -e "$((($(date -ud $dat +%s)-$(date -ud $(date +%Y-%m-%d) +%s))/86400)) \033[1;37mDias\033[0m")
-    fi
-fi
-Usuario=$(printf ' %-15s' "$users")
-Senha=$(printf '%-13s' "$senha")
-Limite=$(printf '%-10s' "$lim")
-Data=$(printf '%-1s' "$data")
-echo -e "\033[1;33m$Usuario \033[1;37m$Senha \033[1;37m$Limite \033[1;32m$Data\033[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-done
-echo ""
-_tuser=$(awk -F: '$3>=1000 {print $1}' /etc/passwd | grep -v nobody | wc -l)
-_ons=$(ps -x | grep sshd | grep -v root | grep priv | wc -l)
-[[ "$(cat /etc/SSHPlus/Exp)" != "" ]] && _expuser=$(cat /etc/SSHPlus/Exp) || _expuser="0"
-[[ -e /etc/openvpn/openvpn-status.log ]] && _onop=$(grep -c "10.8.0" /etc/openvpn/openvpn-status.log) || _onop="0"
-[[ -e /etc/default/dropbear ]] && _drp=$(ps aux | grep dropbear | grep -v grep | wc -l) _ondrp=$(($_drp - 1)) || _ondrp="0"
-_onli=$(($_ons + $_onop + $_ondrp))
-echo -e "\033[1;33m• \033[1;36mTOTAL USUARIOS\033[1;37m $_tuser \033[1;33m• \033[1;32mONLINES\033[1;37m: $_onli \033[1;33m• \033[1;31mVENCIDOS\033[1;37m: $_expuser \033[1;33m•\033[0m"
+drop_port(){
+    local portasVAR=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
+    local NOREPEAT
+    local reQ
+    local Port
+
+    while read port; do
+        reQ=$(echo ${port}|awk '{print $1}')
+        Port=$(echo {$port} | awk '{print $9}' | awk -F ":" '{print $2}')
+        [[ $(echo -e $NOREPEAT|grep -w "$Port") ]] && continue
+        NOREPEAT+="$Port\n"
+
+        case ${reQ} in
+            cupsd|systemd-r|exim4)continue;;
+            *)DPB+=" $reQ:$Port";;
+        esac
+    done <<< "${portasVAR}"
+ }
+
+conf(){
+	[[ $opcion = @(n|N) ]] && return 1
+	drop_port
+	encab
+	print_center -azu "Selecciones puerto de redireccion de trafico"
+	msg -bar
+
+	n=1
+	for i in $DPB; do
+        proto=$(echo $i|awk -F ":" '{print $1}')
+        proto2=$(printf '%-12s' "$proto")
+        port=$(echo $i|awk -F ":" '{print $2}')
+        echo -e " $(msg -verd "[$n]") $(msg -verm2 ">") $(msg -ama "$proto2")$(msg -azu "$port")"
+        drop[$n]=$port
+        num_opc="$n"
+        let n++ 
+    done
+	back
+	opc=$(selection_fun $num_opc)
+	[[ $opc = 0 ]] && return 1
+	encab
+	echo -e "\033[1;33m Puerto de redireccion de trafico: \033[1;32m${drop[$opc]}"
+	msg -bar
+ while [[ -z $opc2 ]]; do
+	echo -ne "\033[1;37m Ingrese un puerto para WEBSOCKET: " && read opc2
+	tput cuu1 && tput dl1
+
+        [[ $(mportas|grep -w "${opc2}") = "" ]] && {
+        	echo -e "\033[1;33m $(fun_trans  "Puerto de websocket:")\033[1;32m ${opc2} OK"
+        	msg -bar
+        } || {
+        	echo -e "\033[1;33m $(fun_trans  "Puerto de websocket:")\033[1;31m ${opc2} FAIL" && sleep 2
+        	tput cuu1 && tput dl1
+        	unset opc2
+        }
+ done
+
+ 	while :
+ 	do
+	echo -ne "\033[1;37m Desea continuar [s/n]: " && read start
+	tput cuu1 && tput dl1
+	if [[ -z $start ]]; then
+		echo -e "\033[1;37m deves ingresar una opcion \033[1;32m[S] \033[1;37mpara Si \033[1;31m[n] \033[1;37mpara No." && sleep 2
+		tput cuu1 && tput dl1
+	else
+		[[ $start = @(n|N) ]] && break
+		if [[ $start = @(s|S) ]]; then
+			node_v="$(which nodejs)" && [[ $(ls -l ${node_v}|grep -w 'node') ]] && node_v="$(which node)"
+echo -e "[Unit]
+Description=P7COM-nodews1
+Documentation=https://p7com.net/
+After=network.target nss-lookup.target\n
+[Service]
+User=nobody
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=${node_v} /etc/ADMRufu/install/WS-Proxy.js -dhost 127.0.0.1 -dport ${drop[$opc]} -mport $opc2
+Restart=on-failure
+RestartSec=3s\n
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/nodews.$opc2.service
+
+			systemctl enable nodews.$opc2 &>/dev/null
+			systemctl start nodews.$opc2 &>/dev/null
+			for ufww in `echo $opc2`; do
+				ufw allow $ufww/tcp > /dev/null 2>&1
+			done
+			print_center -verd "Ejecucion con exito"
+			enter
+			break
+		fi
+	fi
+	done
+	return 1
+ }
+
+stop_ws () {
+	ck_ws=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND"|grep "node")
+	if [[ -z $(echo "$ck_ws" | awk '{print $1}' | head -n 1) ]]; then
+		print_center -verm2 "WEBSOCKET no encontrado"
+	else
+		ck_port=$(echo "$ck_ws" | awk '{print $9}' | awk -F ":" '{print $2}')
+		for i in $ck_port; do
+			systemctl stop nodews.${i} &>/dev/null
+			systemctl disable nodews.${1} &>/dev/null
+			rm /etc/systemd/system/nodews.${i}.service &>/dev/null
+		done
+		print_center -verm2 "WEBSOCKET detenido"	
+	fi
+	enter
+	return 1
+ }
+
+ stop_port () {
+ 	clear
+	STWS=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND"|grep "node" | awk '{print $9}' | awk -F ":" '{print $2}')
+	title "DETENER UN PUERTO"
+ 	n=1
+	for i in $STWS; do
+		echo -e " \033[1;32m[$n] \033[1;31m> \033[1;37m$i\033[0m"
+		wspr[$n]=$i
+		let n++	
+	done
+ 	back
+ 	echo -ne "\033[1;37m opcion: " && read prws
+ 	tput cuu1 && tput dl1
+ 	[[ $prws = "0" ]] && return
+ 	systemctl stop nodews.${wspr[$prws]} &>/dev/null
+	systemctl disable nodews.${wspr[$prws]} &>/dev/null
+	rm /etc/systemd/system/nodews.${wspr[$prws]}.service &>/dev/null
+	print_center -verm2 "PUERTO WEBSOCKET ${wspr[$prws]} detenido"
+ 	enter
+	return 1
+ }
+
+encab(){
+	title "SSH OVER WEBSOCKET CDN CLOUDFLARE"
+ }
+
+encab
+menu_func "INICIAR/AGREGAR PROXY WS CDN" "DETENER PROXY WS CDN"
+
+sf=2
+[[ $(lsof -V -i tcp -P -n|grep -v "ESTABLISHED"|grep -v "COMMAND"|grep "node"|wc -l) -ge "2" ]] && echo -e "$(msg -verd " [3]") $(msg -verm2 ">") $(msg -azu "DETENER UN PUERTO")" && sf=3
+back
+selection=$(selection_fun ${sf})
+case ${selection} in
+	1) conf;;
+	2) stop_ws && read foo;;
+	3) stop_port;;
+	0)return 1;;
+esac
